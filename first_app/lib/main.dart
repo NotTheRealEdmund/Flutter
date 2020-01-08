cfe0a0f00887a2799f1a24735918cb2460c17c53
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-08 17:54:04 +0800

    Add a body widget containing an array of widgets placed in a column, an image and a text

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/anime.jpg'),
+          Text('Anime Image')
+        ],),),
       ),
     );
   }
