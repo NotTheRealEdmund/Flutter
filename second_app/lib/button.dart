5f38c8653a271dbfd8a542c40fc63562ce4578ac
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-10 16:58:34 +0800

    Create a button class

---
+++ second_app/lib/button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Button {
+  final int id;
+  String text;
+  Color bg;
+  bool marked;
+
+  Button({this.id, this.text = '', this.bg = Colors.deepOrange, this.marked = true});
+}
