d83cf8b33811563d90f52a481cb578de179f70ec
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-14 11:43:23 +0800

    Basic online shopping page, extend idea from tic-tac-toe game

---
+++ third_app/lib/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // Nothing for now
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Products Page'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.all(10.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.0,
+                    crossAxisSpacing: 9.0,
+                    mainAxisSpacing: 9.0),
+                itemCount: 18,
+                itemBuilder: (context, i) => SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    onPressed: () {},
+                    child: Text('This is a product.'),
+                  ),
+                ),
+              ),
+            ),
+            RaisedButton(
+                child: Text(
+                  'Add to cart',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Colors.red,
+                padding: const EdgeInsets.all(20.0),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ))
+          ],
+        ));
+  }
+}
