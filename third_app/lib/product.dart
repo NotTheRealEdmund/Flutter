2f8438e29479049a102b107acf5d2bf1625fc4d6
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-15 22:30:10 +0800

    Add more items into product.dart's itemList and cart.dart's orderList

--- third_app/lib/product.dart
+++ third_app/lib/product.dart
@@ -15,7 +15,19 @@ class _ProductState extends State<Product> {
     Item(name: 'Chino figurine', price: '68.00', image: 'figurine_chino.jpg'),
     Item(name: 'Cocoa figurine', price: '69.00', image: 'figurine_cocoa.jpg'),
     Item(name: 'Rize figurine', price: '70.00', image: 'figurine_rize.jpg'),
-    Item(name: 'Syaro figurine', price: '71.00', image: 'figurine_syaro.jpg')
+    Item(name: 'Syaro figurine', price: '71.00', image: 'figurine_syaro.jpg'),
+    Item(name: 'Anime keychain', price: '5.00', image: 'anime_keychain.jpg'),
+    Item(name: 'Ballpoint pen', price: '2.00', image: 'ballpoint_pen.jpg'),
+    Item(name: 'Bottle cap', price: '0.50', image: 'bottle_cap.jpg'),
+    Item(name: 'Gacha amulet', price: '3.00', image: 'gacha_amulet.jpg'),
+    Item(name: 'Nokia phone', price: '30.00', image: 'nokia_phone.jpg'),
+    Item(name: 'Puzzle ring', price: '7.00', image: 'puzzle_ring.jpg'),
+    Item(name: 'Black ring', price: '20.00', image: 'ring_black.jpg'),
+    Item(name: 'Silver ring', price: '20.00', image: 'ring_silver.jpg'),
+    Item(name: 'Sharpie marker', price: '4.50', image: 'sharpie_marker.jpg'),
+    Item(name: 'Tarot card', price: '1.00', image: 'tarot_card.jpg'),
+    Item(name: 'Tennis ball', price: '2.50', image: 'tennis_ball.jpg'),
+    Item(name: 'Wire', price: '0.10', image: 'wire.jpg')
   ];
 
   void addToCart() {
