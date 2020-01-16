22a8c189437e149908527929f5de958e3a5eb71c
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-16 18:43:11 +0800

    Add product cards of hot items to home.dart

--- third_app/lib/home.dart
+++ third_app/lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './item.dart';
+import './product_card.dart';
+
 class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -45,6 +48,33 @@ class _HomeState extends State<Home> {
             ],
           ),
         ),
-        body: Text('This is the home page'));
+        body: ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Hot items!' ,style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400, color: Colors.red)),
+                  ),
+                ),
+              ],
+            ),
+            ProductCard(
+              item: Item(name: 'Chino figurine', price: '68.00', image: 'figurine_chino.jpg'),
+            ),
+            ProductCard(
+              item: Item(name: 'Cocoa figurine', price: '69.00', image: 'figurine_cocoa.jpg'),
+            ),
+            ProductCard(
+              item: Item(name: 'Rize figurine', price: '70.00', image: 'figurine_rize.jpg'),
+            ),
+            ProductCard(
+              item: Item(name: 'Syaro figurine', price: '71.00', image: 'figurine_syaro.jpg'),
+            ),
+          ],
+        ),
+    );
   }
 }
